86077c7a7188ddd6b0cb87be2805303fc2b976d5
A♂圣旭 <huajieshen@126.com>
2017-10-21 21:35:50 +0800

    完善

--- src/main/java/org/ibase4j/core/support/cache/RedisHelper.java
+++ src/main/java/org/ibase4j/core/support/cache/RedisHelper.java
@@ -37,11 +37,13 @@ public final class RedisHelper implements CacheManager {
         CacheUtil.setCacheManager(this);
     }
 
+    @Override
     public final Object get(final String key) {
         expire(key, EXPIRE);
         return redisTemplate.boundValueOps(key).get();
     }
 
+    @Override
     public final Set<Object> getAll(final String pattern) {
         Set<Object> values = InstanceUtil.newHashSet();
         Set<Serializable> keys = redisTemplate.keys(pattern);
@@ -52,28 +54,34 @@ public final class RedisHelper implements CacheManager {
         return values;
     }
 
+    @Override
     public final void set(final String key, final Serializable value, int seconds) {
         redisTemplate.boundValueOps(key).set(value);
         expire(key, seconds);
     }
 
+    @Override
     public final void set(final String key, final Serializable value) {
         redisTemplate.boundValueOps(key).set(value);
         expire(key, EXPIRE);
     }
 
+    @Override
     public final Boolean exists(final String key) {
         return redisTemplate.hasKey(key);
     }
 
+    @Override
     public final void del(final String key) {
         redisTemplate.delete(key);
     }
 
+    @Override
     public final void delAll(final String pattern) {
         redisTemplate.delete(redisTemplate.keys(pattern));
     }
 
+    @Override
     public final String type(final String key) {
         expire(key, EXPIRE);
         return redisTemplate.type(key).getClass().getName();
@@ -81,43 +89,48 @@ public final class RedisHelper implements CacheManager {
 
     /**
      * 在某段时间后失效
-     * 
      * @return
      */
+    @Override
     public final Boolean expire(final String key, final int seconds) {
         return redisTemplate.expire(key, seconds, TimeUnit.SECONDS);
     }
 
     /**
      * 在某个时间点失效
-     * 
      * @param key
      * @param unixTime
      * @return
      */
+    @Override
     public final Boolean expireAt(final String key, final long unixTime) {
         return redisTemplate.expireAt(key, new Date(unixTime));
     }
 
+    @Override
     public final Long ttl(final String key) {
         return redisTemplate.getExpire(key, TimeUnit.SECONDS);
     }
 
+    @Override
     public final void setrange(final String key, final long offset, final String value) {
         redisTemplate.boundValueOps(key).set(value, offset);
         expire(key, EXPIRE);
     }
 
+    @Override
     public final String getrange(final String key, final long startOffset, final long endOffset) {
         expire(key, EXPIRE);
         return redisTemplate.boundValueOps(key).get(startOffset, endOffset);
     }
 
+    @Override
     public final Object getSet(final String key, final Serializable value) {
         expire(key, EXPIRE);
         return redisTemplate.boundValueOps(key).getAndSet(value);
     }
 
+    @Override
     public boolean setnx(String key, Serializable value) {
         RedisConnectionFactory factory = redisTemplate.getConnectionFactory();
         RedisConnection redisConnection = null;
@@ -134,6 +147,7 @@ public final class RedisHelper implements CacheManager {
         }
     }
 
+    @Override
     public boolean lock(String key) {
         RedisConnectionFactory factory = redisTemplate.getConnectionFactory();
         RedisConnection redisConnection = null;
@@ -148,20 +162,29 @@ public final class RedisHelper implements CacheManager {
         }
     }
 
+    @Override
     public void unlock(String key) {
         redisTemplate.delete(key);
     }
 
+    @Override
     public void hset(String key, Serializable field, Serializable value) {
         redisTemplate.boundHashOps(key).put(field, value);
     }
 
+    @Override
     public Object hget(String key, Serializable field) {
         return redisTemplate.boundHashOps(key).get(field);
     }
 
+    @Override
     public void hdel(String key, Serializable field) {
         redisTemplate.boundHashOps(key).delete(field);
+    }
+
+    @Override
+    public Long incr(String key) {
+        return redisTemplate.boundValueOps(key).increment(1L);
     }
 
     // 未完，待续...
--- src/main/java/org/ibase4j/core/support/cache/RedissonHelper.java
+++ src/main/java/org/ibase4j/core/support/cache/RedissonHelper.java
@@ -32,18 +32,21 @@ public class RedissonHelper implements CacheManager {
         return redissonClient.getBucket(key);
     }
 
+    @Override
     public final Object get(final String key) {
         RBucket<Object> temp = getRedisBucket(key);
         expire(temp, EXPIRE);
         return temp.get();
     }
 
+    @Override
     public final void set(final String key, final Serializable value) {
         RBucket<Object> temp = getRedisBucket(key);
         temp.set(value);
         expire(temp, EXPIRE);
     }
 
+    @Override
     public final void set(final String key, final Serializable value, int seconds) {
         RBucket<Object> temp = getRedisBucket(key);
         temp.set(value);
@@ -54,19 +57,23 @@ public class RedissonHelper implements CacheManager {
         redissonClient.getBuckets().set(temps);
     }
 
+    @Override
     public final Boolean exists(final String key) {
         RBucket<Object> temp = getRedisBucket(key);
         return temp.isExists();
     }
 
+    @Override
     public final void del(final String key) {
         redissonClient.getKeys().delete(key);
     }
 
+    @Override
     public final void delAll(final String pattern) {
         redissonClient.getKeys().deleteByPattern(pattern);
     }
 
+    @Override
     public final String type(final String key) {
         RType type = redissonClient.getKeys().getType(key);
         if (type == null) {
@@ -76,45 +83,38 @@ public class RedissonHelper implements CacheManager {
     }
 
     /**
-     * 
      * 在某段时间后失效
-     *
-     * 
-     * 
      * @return
-     * 
      */
     private final void expire(final RBucket<Object> bucket, final int seconds) {
         bucket.expire(seconds, TimeUnit.SECONDS);
     }
 
     /**
-     * 
      * 在某个时间点失效
-     *
-     * 
-     * 
      * @param key
-     * 
      * @param unixTime
-     * 
      * @return
      * 
      */
+    @Override
     public final Boolean expireAt(final String key, final long unixTime) {
         return redissonClient.getBucket(key).expireAt(new Date(unixTime));
     }
 
+    @Override
     public final Long ttl(final String key) {
         RBucket<Object> rBucket = getRedisBucket(key);
         return rBucket.remainTimeToLive();
     }
 
+    @Override
     public final Object getSet(final String key, final Serializable value) {
         RBucket<Object> rBucket = getRedisBucket(key);
         return rBucket.getAndSet(value);
     }
 
+    @Override
     public Set<Object> getAll(String pattern) {
         Set<Object> set = InstanceUtil.newHashSet();
         Iterable<String> keys = redissonClient.getKeys().getKeysByPattern(pattern);
@@ -125,20 +125,24 @@ public class RedissonHelper implements CacheManager {
         return set;
     }
 
+    @Override
     public Boolean expire(String key, int seconds) {
         RBucket<Object> bucket = getRedisBucket(key);
         expire(bucket, seconds);
         return true;
     }
 
+    @Override
     public void hset(String key, Serializable field, Serializable value) {
         redissonClient.getMap(key).put(field, value);
     }
 
+    @Override
     public Object hget(String key, Serializable field) {
         return redissonClient.getMap(key).get(field);
     }
 
+    @Override
     public void hdel(String key, Serializable field) {
         redissonClient.getMap(key).remove(field);
     }
@@ -155,19 +159,37 @@ public class RedissonHelper implements CacheManager {
         return redissonClient.getSet(key).remove(value);
     }
 
+    @Override
     public boolean lock(String key) {
         return redissonClient.getLock(key).tryLock();
     }
 
+    @Override
     public void unlock(String key) {
         redissonClient.getLock(key).unlock();
     }
 
+    @Override
     public boolean setnx(String key, Serializable value) {
         try {
             return redissonClient.getLock(key).tryLock(Long.valueOf(value.toString()), TimeUnit.MILLISECONDS);
         } catch (InterruptedException e) {
             return false;
         }
+    }
+
+    @Override
+    public Long incr(String key) {
+        return null;
+    }
+
+    @Override
+    public void setrange(String key, long offset, String value) {
+
+    }
+
+    @Override
+    public String getrange(String key, long startOffset, long endOffset) {
+        return null;
     }
 }
