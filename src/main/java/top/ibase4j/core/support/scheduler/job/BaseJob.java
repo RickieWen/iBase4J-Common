65ca7ca8a30556c0b85e12141080dbd83bfbb256
A♂圣翔 <huajieshen@126.com>
2017-11-11 12:53:42 +0800

    修改调度bug

--- src/main/java/top/ibase4j/core/support/scheduler/job/BaseJob.java
+++ src/main/java/top/ibase4j/core/support/scheduler/job/BaseJob.java
@@ -15,7 +15,6 @@ import top.ibase4j.core.base.BaseProvider;
 import top.ibase4j.core.base.Parameter;
 import top.ibase4j.core.support.scheduler.TaskScheduled.TaskType;
 import top.ibase4j.core.util.CacheUtil;
-import top.ibase4j.core.util.DubboUtil;
 
 /**
  * 默认调度(非阻塞)
@@ -24,51 +23,51 @@ import top.ibase4j.core.util.DubboUtil;
  * @version 2016年12月29日 上午11:52:32
  */
 public class BaseJob implements Job {
-	private final Logger logger = LoggerFactory.getLogger(BaseJob.class);
+    private final Logger logger = LoggerFactory.getLogger(BaseJob.class);
 
-	public void execute(JobExecutionContext context) throws JobExecutionException {
-		long start = System.currentTimeMillis();
-		JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
-		String taskType = jobDataMap.getString("taskType");
-		String targetObject = jobDataMap.getString("targetObject");
-		String targetMethod = jobDataMap.getString("targetMethod");
-		String key = targetMethod + "." + targetObject;
-		try {
-			logger.info("定时任务[{}.{}]开始", targetObject, targetMethod);
-			if (CacheUtil.getCache().lock(key)) {
-				try {
-					ApplicationContext applicationContext = (ApplicationContext) context.getScheduler().getContext()
-							.get("applicationContext");
-					if (TaskType.local.equals(taskType)) {
-						Object refer = applicationContext.getBean(targetObject);
-						refer.getClass().getDeclaredMethod(targetMethod).invoke(refer);
-					} else if (TaskType.dubbo.equals(taskType)) {
-						String system = "org.ibase4j.provider.I" + jobDataMap.getString("targetSystem");
-						BaseProvider provider = (BaseProvider) DubboUtil.refer(applicationContext, system);
-						provider.execute(new Parameter(targetObject, targetMethod));
-					}
-					double time = (System.currentTimeMillis() - start) / 1000.0;
-					logger.info("定时任务[{}.{}]用时：{}s", targetObject, targetMethod, time);
-				} finally {
-					unLock(key);
-				}
-			}
-		} catch (Exception e) {
-			throw new JobExecutionException(e);
-		}
-	}
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        long start = System.currentTimeMillis();
+        JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
+        String taskType = jobDataMap.getString("taskType");
+        String targetObject = jobDataMap.getString("targetObject");
+        String targetMethod = jobDataMap.getString("targetMethod");
+        String key = targetMethod + "." + targetObject;
+        try {
+            logger.info("定时任务[{}.{}]开始", targetObject, targetMethod);
+            if (CacheUtil.getCache().lock(key)) {
+                try {
+                    ApplicationContext applicationContext = (ApplicationContext)context.getScheduler().getContext()
+                        .get("applicationContext");
+                    if (TaskType.local.equals(taskType)) {
+                        Object refer = applicationContext.getBean(targetObject);
+                        refer.getClass().getDeclaredMethod(targetMethod).invoke(refer);
+                    } else if (TaskType.dubbo.equals(taskType)) {
+                        BaseProvider provider = (BaseProvider)applicationContext
+                            .getBean(jobDataMap.getString("targetSystem"));
+                        provider.execute(new Parameter(targetObject, targetMethod));
+                    }
+                    double time = (System.currentTimeMillis() - start) / 1000.0;
+                    logger.info("定时任务[{}.{}]用时：{}s", targetObject, targetMethod, time);
+                } finally {
+                    unLock(key);
+                }
+            }
+        } catch (Exception e) {
+            throw new JobExecutionException(e);
+        }
+    }
 
-	private void unLock(String key) {
-		try {
-			CacheUtil.getCache().del(key);
-		} catch (Exception e) {
-			logger.error("", e);
-			try {
-				Thread.sleep(1000);
-			} catch (Exception e2) {
-				logger.error("", e2);
-			}
-			unLock(key);
-		}
-	}
+    private void unLock(String key) {
+        try {
+            CacheUtil.getCache().del(key);
+        } catch (Exception e) {
+            logger.error("", e);
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e2) {
+                logger.error("", e2);
+            }
+            unLock(key);
+        }
+    }
 }
