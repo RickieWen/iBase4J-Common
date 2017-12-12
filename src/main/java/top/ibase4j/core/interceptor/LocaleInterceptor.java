beda5037ed5c79c3371a80233ca61a3a81598ebc
A♂圣翔 <huajieshen@126.com>
2017-12-12 19:35:36 +0800

    优化

--- src/main/java/top/ibase4j/core/interceptor/LocaleInterceptor.java
+++ src/main/java/top/ibase4j/core/interceptor/LocaleInterceptor.java
@@ -1,6 +1,5 @@
 package top.ibase4j.core.interceptor;
 
-import java.io.IOException;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,9 +9,6 @@ import javax.servlet.http.HttpSession;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.i18n.LocaleContextHolder;
 
-import cz.mallat.uasparser.OnlineUpdater;
-import cz.mallat.uasparser.UASparser;
-
 /**
  * 国际化信息设置(基于SESSION)
  * 
@@ -20,16 +16,6 @@ import cz.mallat.uasparser.UASparser;
  * @version 2016年5月20日 下午3:16:45
  */
 public class LocaleInterceptor extends BaseInterceptor {
-    static UASparser uasParser;
-
-    static {
-        try {
-            uasParser = new UASparser(OnlineUpdater.getVendoredInputStream());
-        } catch (IOException e) {
-            logger.error("", e);
-        }
-    }
-
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
         throws Exception {
         HttpSession session = request.getSession();
