d89b750b98d77d159179249f569ff89f31f8c813
A♂圣翔 <huajieshen@126.com>
2017-12-12 19:31:17 +0800

    Revert "优化"
    This reverts commit 61c292049297f2e980beb1a443c08d0bdaad84ce.

--- src/main/java/top/ibase4j/core/interceptor/LocaleInterceptor.java
+++ src/main/java/top/ibase4j/core/interceptor/LocaleInterceptor.java
@@ -1,22 +1,17 @@
-package top.ibase4j.core.filter;
+package top.ibase4j.core.interceptor;
 
 import java.io.IOException;
 import java.util.Locale;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.i18n.LocaleContextHolder;
 
-import top.ibase4j.core.Constants;
-import top.ibase4j.core.util.WebUtil;
+import cz.mallat.uasparser.OnlineUpdater;
+import cz.mallat.uasparser.UASparser;
 
 /**
  * 国际化信息设置(基于SESSION)
@@ -24,14 +19,20 @@ import top.ibase4j.core.util.WebUtil;
  * @author ShenHuaJie
  * @version 2016年5月20日 下午3:16:45
  */
-public class LocaleFilter implements Filter {
-    public void init(FilterConfig filterConfig) throws ServletException {
+public class LocaleInterceptor extends BaseInterceptor {
+    static UASparser uasParser;
+
+    static {
+        try {
+            uasParser = new UASparser(OnlineUpdater.getVendoredInputStream());
+        } catch (IOException e) {
+            logger.error("", e);
+        }
     }
 
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-        throws IOException, ServletException {
-        HttpServletRequest req = (HttpServletRequest)request;
-        HttpSession session = req.getSession();
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+        throws Exception {
+        HttpSession session = request.getSession();
         // 设置客户端语言
         Locale locale = (Locale)session.getAttribute("LOCALE");
         if (locale == null) {
@@ -44,12 +45,6 @@ public class LocaleFilter implements Filter {
             }
         }
         LocaleContextHolder.setLocale(locale);
-
-        session.setAttribute(Constants.USER_IP, WebUtil.getHost(req));
-        chain.doFilter(request, response);
-    }
-
-    public void destroy() {
-
+        return super.preHandle(request, response, handler);
     }
 }
