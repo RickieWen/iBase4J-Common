e42b11d74112f19125f7747e18260da590e4034a
A♂圣翔 <huajieshen@126.com>
2017-12-24 16:37:58 +0800

    优化

--- src/main/java/top/ibase4j/core/util/CacheUtil.java
+++ src/main/java/top/ibase4j/core/util/CacheUtil.java
@@ -1,5 +1,8 @@
 package top.ibase4j.core.util;
 
+import org.apache.commons.lang3.StringUtils;
+
+import top.ibase4j.core.exception.BusinessException;
 import top.ibase4j.core.support.cache.CacheManager;
 
 public class CacheUtil {
@@ -36,5 +39,32 @@ public class CacheUtil {
     /** 解锁 */
     public static void unlock(String key) {
         lockManager.unlock(key);
+    }
+
+    /**
+     * 次数检查
+     * @param key
+     * @param seconds 缓存时间
+     * @param frequency 最多次数
+     * @param message 超出次数提示信息
+     */
+    public static void refreshTimes(String key, int seconds, int frequency, String message) {
+        if (CacheUtil.getLock(key + "-LOCK")) {
+            try {
+                Integer times = 1;
+                String timesStr = (String)CacheUtil.getCache().get(key);
+                if (StringUtils.isNotBlank(timesStr)) {
+                    times = Integer.valueOf(timesStr) + 1;
+                    if (times > frequency) {
+                        throw new BusinessException(message);
+                    }
+                }
+                CacheUtil.getCache().set(key, times.toString(), seconds);
+            } finally {
+                CacheUtil.unlock(key + "-LOCK");
+            }
+        } else {
+            refreshTimes(key, seconds, frequency, message);
+        }
     }
 }
